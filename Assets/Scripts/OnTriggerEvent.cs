f23a903c38e25047c6024b743f41f8080acdf37a
fukada <fukada.m888@gmail.com>
2025-05-07 22:21:29 +0900

    プレイヤーが接近すると追従

--- Assets/Scripts/OnTriggerEvent.cs
+++ Assets/Scripts/OnTriggerEvent.cs
@@ -17,22 +17,41 @@ public class OnTriggerEvent : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log($"[Trigger Enter] {other.gameObject.name} が入りました");
-        
-        if (other.tag == "Player"){
-            target = other.transform;
-        }
+        target = null;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log($"[Trigger Enter] {collision.gameObject.name} が入りました");
+        target = null;
     }
 
     // トリガーから出た瞬間に呼ばれる
     private void OnTriggerExit(Collider other)
     {
         Debug.Log($"[Trigger Exit] {other.gameObject.name} が出ました");
+        if (other.tag == "Player"){
+            target = other.transform;
+        }
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+         Debug.Log($"[Trigger Exit] {collision.gameObject.name} が出ました");
+        if (collision.gameObject.tag == "Player"){
+            target = collision.transform;
+        }
     }
 
     void Update()
     {
         if (target != null){
             agent.SetDestination(target.position);
+        }
+        if (target != null && Vector3.Distance(transform.position, target.position) > 3){
+            agent.isStopped = false;
+        }else if(target != null && Vector3.Distance(transform.position, target.position) <= 3) {
+            agent.isStopped = true;
         }
     }
 }
