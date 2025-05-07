912aebf1136b25c72652c5e8d103488940c2daed
fukada <fukada.m888@gmail.com>
2025-05-08 08:42:54 +0900

    クリック位置を取得するUtilの作成

---
+++ Assets/Scripts/Util/MousePosition.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class MousePosition
+{
+    // クリック位置の座標を返す。レイが何も当たらなかったときは引数でもらった現在の座標をそのまま返す。
+    public Vector3 GetMousePosition(Transform currentTransform){
+         // 1) 画面上のクリック位置からレイを飛ばす。レイキャストの最大距離は100f
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        // 2) レイキャストして当たったポイントを取得
+        if (Physics.Raycast(ray, out RaycastHit hitInfo, 100f))
+        {
+            Vector3 hitPoint = hitInfo.point;
+            Debug.Log($"クリック（3D）したワールド座標: {hitPoint}");
+            Debug.Log($"当たったオブジェクト: {hitInfo.collider.name}");
+            return hitPoint;
+        }
+        else
+        {
+            Debug.Log("何にも当たりませんでした");
+            // 現在の座標
+            return currentTransform.position;
+        }
+    }
+}
