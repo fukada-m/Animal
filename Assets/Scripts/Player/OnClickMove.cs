084f4726fdc971ec523dea8cc6c26ffa68cb6d05
fukada <fukada.m888@gmail.com>
2025-05-08 08:43:58 +0900

    Playerに関する操作をするフォルダを作成

--- Assets/Scripts/Player/OnClickMove.cs
+++ Assets/Scripts/Player/OnClickMove.cs
@@ -9,7 +9,8 @@ public class OnClickMove : MonoBehaviour
 
     [SerializeField]
     float moveSpeed;
-
+    [SerializeField]
+    MousePosition mousePosition;
     Vector3 movePosition;
 
     void Awake(){
@@ -26,27 +27,7 @@ public class OnClickMove : MonoBehaviour
 
     // クリックされた座標を元にUpdate()内でターゲットを移動させている。
     void SetMovePosition(){
-        movePosition = GetMousePosition();
-    }
-
-    // クリックした座標のVector3を返す。
-    Vector3 GetMousePosition(){
-        // 1) 画面上のクリック位置からレイを飛ばす。レイキャストの最大距離は100f
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-        // 2) レイキャストして当たったポイントを取得
-        if (Physics.Raycast(ray, out RaycastHit hitInfo, 100f))
-        {
-            Vector3 hitPoint = hitInfo.point;
-            Debug.Log($"クリック（3D）したワールド座標: {hitPoint}");
-            Debug.Log($"当たったオブジェクト: {hitInfo.collider.name}");
-            return hitPoint;
-        }
-        else
-        {
-            Debug.Log("何にも当たりませんでした");
-            return transform.position;
-        }
+        movePosition = mousePosition.GetMousePosition(transform);
     }
 
     void Update(){
