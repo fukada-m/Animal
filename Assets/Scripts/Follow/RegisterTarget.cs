79e4b9306b95a48e0574f52d740f89edae726fa5
fukada <fukada.m888@gmail.com>
2025-05-13 21:33:41 +0900

    Frendがplayerをターゲットに登録して追跡する

--- Assets/Scripts/Follow/RegisterTarget.cs
+++ Assets/Scripts/Follow/RegisterTarget.cs
@@ -1,61 +1,20 @@
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.AI;
 
 public class RegisterTarget : MonoBehaviour
 {
-    readonly float targetInterval = 3;
-    NavMeshAgent agent;
-
-    Transform target;
-
-    GameObject currentLeader;
+    Frend frend;
 
     void Start(){
-        agent = GetComponent<NavMeshAgent>();
-        if (agent == null){
-            throw new System.Exception("Nav Mesh Agentをアタッチしてください");
-        }
+        frend = GetComponent<Frend>();
     }
     // TODOUniRXを使ってイベント化する
-    // リーダーを登録してリーダーの最後尾にいるオブジェクトについていく
+    // プレイヤーをターゲットとして登録する
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log($"[Trigger Enter] {other.gameObject.name} が入りました");
         if (other.gameObject.tag == "Player"){
-            // リーダーが変わっていればをリーダーを交代してその最後尾を自分のターゲットにセットする。そして、自分が最後尾になる。
-            if (IsChange(other.gameObject, currentLeader)){
-                currentLeader = other.gameObject;
-                var player = currentLeader.GetComponent<Player>();
-                if(player == null){throw new System.Exception("PlayerにPlayerクラスをアタッチしよう");}
-                target = player.LastFrend.transform;
-                player.LastFrend = this.gameObject;
-            }
+            frend.Target = other.transform;
         }
     }
 
-    bool IsChange(GameObject leader, GameObject currentleader){
-        if(currentLeader == null)return true;
-        if (currentleader == leader){
-            return false;
-        } else{
-            return true;
-        }
-    }
-
-    void Update()
-    {
-        Fllow();
-    }
-
-    // targetとの距離がtargetInterval以上離れたら追跡する。
-    void Fllow(){
-        if (target == null) {return;}
-        agent.SetDestination(target.position);
-        if (Vector3.Distance(transform.position, target.position) > targetInterval){
-            agent.isStopped = false;
-        }else {
-            agent.isStopped = true;
-        }
-    }
 }
---
+++ Assets/Scripts/Frend.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using Fusion;
+using UnityEngine.AI;
+
+public class Frend : NetworkBehaviour
+{
+    public Transform Target {get; set;}
+    NavMeshAgent agent;
+    readonly float targetInterval = 3;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        if (agent == null){
+            throw new System.Exception("Nav Mesh Agentをアタッチしてください");
+        }
+    }
+
+    // Update is called once per frame
+    public override void FixedUpdateNetwork()
+    {
+        Fllow();
+    }
+
+    // targetとの距離がtargetInterval以上離れたら追跡する。
+    void Fllow(){
+        if (Target == null) {return;}
+        agent.SetDestination(Target.position);
+        if (Vector3.Distance(transform.position, Target.position) > targetInterval){
+            agent.isStopped = false;
+        }else {
+            agent.isStopped = true;
+        }
+    }
+}
